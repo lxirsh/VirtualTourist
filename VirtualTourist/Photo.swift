51d69c8e5000e098c6d5243ea76df16cee488b62
Lance Hirsch <lxirsh@gmail.com>
2016-07-03 19:28:47 +0300

    Allow external storage for image attribute of photo entity.

---
+++ VirtualTourist/Photo.swift
@@ -0,0 +1,15 @@
+//
+//  Photo.swift
+//  VirtualTourist
+//
+//  Created by Lance Hirsch on 7/3/16.
+//  Copyright © 2016 Lance Hirsch. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Photo: NSManagedObject {
+
+}
---
+++ VirtualTourist/Pin.swift
@@ -0,0 +1,17 @@
+//
+//  Pin.swift
+//  VirtualTourist
+//
+//  Created by Lance Hirsch on 7/3/16.
+//  Copyright © 2016 Lance Hirsch. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Pin: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
