3beb686ed4f1f931de3a7b89c818675957c4da88
Lance Hirsch <lxirsh@gmail.com>
2016-07-05 06:27:51 +0300

    Add a long touch gesture recognizer and a corresponding pin annotation to the map view.

--- VirtualTourist/MapViewController.swift
+++ VirtualTourist/MapViewController.swift
@@ -9,15 +9,30 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, UIGestureRecognizerDelegate {
+    
     @IBOutlet weak var mapView: MKMapView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        var gestureRecognizer = UILongPressGestureRecognizer(target: self, action: "addAnnotation:")
+        gestureRecognizer.minimumPressDuration = 0.5
+        mapView.addGestureRecognizer(gestureRecognizer)
+        
     }
 
+    func addAnnotation(gestureRecognizer: UIGestureRecognizer) {
+        if gestureRecognizer.state == UIGestureRecognizerState.Began {
+            var touchPoint = gestureRecognizer.locationInView(mapView)
+            var newCoordinates = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinates
+            mapView.addAnnotation(annotation)
+            
+        }
+    }
     
 }
 
