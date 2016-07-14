e38f997e88bc1dbba3e2ca501d45dbcd1b01d4b2
Lance Hirsch <lxirsh@gmail.com>
2016-07-14 19:22:24 +0300

    Resize the collection view cells.

--- VirtualTourist/VTImagesViewController.swift
+++ VirtualTourist/VTImagesViewController.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class VTImagesViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
     let reuseIdentifier = "cell"
     var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Configure the layout
+        let space: CGFloat = 1.5
+        let dimension = self.view.frame.width <= self.view.frame.height ? (self.view.frame.width - (2 * space)) / 3.0 : (self.view.frame.width - (2 * space)) / 5.0
+        
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSizeMake(dimension, dimension)
     }
 
     // MARK: - UICollectionViewDataSource protocol
